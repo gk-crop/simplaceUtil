25fd00b591dd5fc127c3eadecc829e771bb67b4e
gk <gk@example.com>
2022-08-13 15:23:49 +0200

    Version 0.0.2 - experimental shiny app

---
+++ inst/shiny/gui/app.R
@@ -0,0 +1,46 @@
+library(shiny)
+library(shinyFiles)
+library(simplaceUtil)
+
+ui <- fluidPage(
+  titlePanel("Convert Sunscan file"),
+  sidebarLayout(
+    
+    sidebarPanel(
+      shinyFilesButton('files', label='File select', title='Please select a file', multiple=FALSE),
+      
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Graph",
+                
+                 
+                 DiagrammeR::grVizOutput("diagram2",width = "100%", height="90%"))
+      )
+    )
+    
+  )
+  
+)
+
+# Define server logic 
+server <- function(input, output) {
+  shinyFileChoose(input, 'files', roots=getVolumes(), filetypes=c('', 'xml'))
+  
+  output$diagram2 <- 
+      
+
+      DiagrammeR::renderGrViz({
+        fn <- parseFilePaths(getVolumes(),input$files)$datapath[1]
+        if(!is.na(fn))
+        {
+          dg <- simplaceUtil::solutionToGraph(fn)
+          DiagrammeR::render_graph(dg)
+        }
+      })
+    
+ }
+  
+# Run the application 
+shinyApp(ui = ui, server = server)
