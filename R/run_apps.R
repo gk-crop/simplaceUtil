b3f49ddf77b72961a02fbadcbf4ad7219e01ac99
gk <gk@uni-bonn.de>
2025-05-21 15:43:35 +0200

    Version 0.8.1 - improved custom shiny app

--- R/run_apps.R
+++ R/run_apps.R
@@ -18,20 +18,28 @@ runSimplaceGuiApp <- function(...)
 #' @param simplacedirs a list containing the named elements instdir, workdir, outdir and options
 #' @param solution filename of a solution (or solution as text)
 #' @param project filename of a project
-#' @param paramlist list of named parameter elements
 #' @param plotlist list of named functions that produce a plot
+#' @param datalist list of named functions that transform output dataframes
+#' @param paramlist list of named parameter elements
+#' @param paramtransform function to transform the parameterlist
 #' @param ... parameters passed to shiny::runApp
 #' @return does not return a value, called for the side effect of running the shiny app
 #' @export
 #'
-runSimplaceCustomGuiApp <- function(simplacedirs, solution, project, paramlist, plotlist, ...)
+runSimplaceCustomGuiApp <- function(simplacedirs, solution, project,
+                                    plotlist=list("Default"=simplaceUtil::plotScalarOutput),
+                                    datalist=list("Default"=identity),
+                                    paramlist=list(),
+                                    paramtransform = identity,
+                                    ...)
 {
-  shiny::shinyOptions(simplacedirs=simplacedirs)
-  shiny::shinyOptions(solutions=solution)
-  shiny::shinyOptions(projects=project)
-  shiny::shinyOptions(paramlist=paramlist)
-  shiny::shinyOptions(plotlist=plotlist)
-  #shiny::runApp(appDir = "inst/shiny/customgui/",...)
+  shiny::shinyOptions(simplacedirs = simplacedirs)
+  shiny::shinyOptions(solutions = solution)
+  shiny::shinyOptions(projects = project)
+  shiny::shinyOptions(plotlist = plotlist)
+  shiny::shinyOptions(datalist = datalist)
+  shiny::shinyOptions(paramlist = paramlist)
+  shiny::shinyOptions(paramtransform = paramtransform )
   shiny::runApp(appDir = system.file("shiny","customgui", package = "simplaceUtil"),...)
 }
 
--- inst/shiny/customgui/app.R
+++ inst/shiny/customgui/app.R
@@ -8,7 +8,15 @@ sd <- getShinyOption("simplacedirs")
 solutionlist <- getShinyOption("solutions")
 projectlist <- getShinyOption("projects")
 paramlist <- getShinyOption("paramlist")
+paramtransform <- getShinyOption("paramtransform")
 plotlist <- getShinyOption("plotlist")
+datalist <- getShinyOption("datalist")
+
+
+tableOptions <- list(
+  lengthMenu = list(c(10,25,100,200,-1),c("10","25","100","200","All")),
+  pageLength=25
+)
 
 
 sp <- initSimplace(sd$instdir, sd$workdir, sd$outdir, javaparameters=sd$options)
@@ -23,16 +31,28 @@ ui <- fluidPage(
   titlePanel("Simplace R GUI"),
   sidebarLayout(
     sidebarPanel(
-  textOutput("test"),
-  uiOutput("inputs"),
-  checkboxInput("queue","Keep results from previous simulations", value=FALSE)
+      textOutput("test"),
+      uiOutput("inputs"),
+      checkboxInput("queue","Keep results from previous simulations", value=FALSE),
+      uiOutput("outselect"),
+      width=3
 
 
     ),
     mainPanel(
-      uiOutput("outselect"),
-      uiOutput("variables"),
-      uiOutput("plots")
+      tabsetPanel(
+        tabPanel("Graphs",
+          uiOutput("variables"),
+          uiOutput("plots")
+        ),
+        tabPanel("Data",
+          uiOutput("outdata")
+        ),
+        tabPanel("Run Parameters",
+          dataTableOutput("outruns"),
+          downloadButton("dl_outruns","Save Run Parameters")
+        )
+      )
     )
   )
 )
@@ -56,6 +76,7 @@ server <- function(input, output) {
   v$start <- NULL
   v$end <- NULL
 
+  v$runs <- NULL
 
   elem <- getElementsFromSolutionFile(solutionlist)
   outids <- getMemoryOutputIds(elem$components)
@@ -63,7 +84,6 @@ server <- function(input, output) {
   v$outids <- outids
   v$outid <- outids[1]
 
-  v$sq <- NULL
 
 
   param <- reactive({
@@ -76,16 +96,15 @@ server <- function(input, output) {
 
   alldata <- reactive({
     if(length(v$outids)>0) {
-      par <- param()
+      par <- paramtransform(param())
 
       output$test <- renderText("running simulation")
       if(!input$queue) {
         resetSimulationQueue(sp)
-        v$sq <- NULL
       }
 
 
-      createSimulation(sp,par,queue = input$queue)
+      simid = createSimulation(sp,par,queue = input$queue)
       runSimulations(sp)
       df <- list()
       for(outid in v$outids) {
@@ -97,12 +116,19 @@ server <- function(input, output) {
       output$test <- renderText("simulation finished")
       if(!input$queue) {
         resetSimulationQueue(sp)
+        if(!is.null(isolate(v$runs))) {
+          isolate(v$runs$hasdata <-FALSE)
+        }
       }
-      else {
-        v$sq <- c(paste(paste(names(par),par,sep=":"),collapse=";"),isolate(v$sq))
+      if(length(par)>0) {
+        run <- as.data.frame(par)
+        run$simulationid <- simid
+        run$hasdata <- TRUE
+        v$runs <- rbind(isolate(v$runs),run)
       }
+
+
       print(paste("ran sim",unique(df[[1]]$simulationid)))
-      print(v$sq)
       df
     }
 
@@ -163,12 +189,13 @@ server <- function(input, output) {
     )}
   )
 
-  # outputs
+  # plots
   tl <- list()
   for(i in seq_along(plotlist)) {
     n <- names(plotlist)[[i]]
     id = paste0("plot_",n)
-    tl[[i]]<-plotOutput(id)
+    tl[[2*i-1]]<-h5(n)
+    tl[[2*i]]<-plotOutput(id)
   }
   output$plots <- renderUI(do.call(tagList,tl))
 
@@ -177,9 +204,51 @@ server <- function(input, output) {
       n <- names(plotlist)[[i]]
       id = paste0("plot_",n)
       output[[id]] <- renderPlot({plotlist[[n]](data(),input$xvariable, input$yvariable,
+                                                NULL,
                                                 input$start, input$end)})
     })
   }
+
+
+  # tables
+  tlt <- list()
+  for(i in seq_along(datalist)) {
+    n <- names(datalist)[[i]]
+    id = paste0("data_",n)
+    tlt[[3*i-2]]<-h5(n)
+    tlt[[3*i-1]]<-dataTableOutput(id)
+    tlt[[3*i]]<-downloadButton(paste0("dl_",id),"Save Data")
+
+  }
+  output$outdata <- renderUI(do.call(tagList,tlt))
+
+  for(i in seq_along(datalist)) {
+    local({
+      n <- names(datalist)[[i]]
+      id = paste0("data_",n)
+      output[[id]] <- renderDataTable(datalist[[n]](data()),options=tableOptions)
+      output[[paste0("dl_",id)]]<-downloadHandler(
+        filename = function() {
+          paste(input$outid,"_",id,"_", format(Sys.time(),format="%Y%m%d_%H%M%S"), ".csv", sep="")
+        },
+        content = function(file) {
+          write.csv(data(), file)
+        }
+      )
+    })
+  }
+
+  output[["dl_outruns"]]<-downloadHandler(
+    filename = function() {
+      paste("RunParameters","_", format(Sys.time(),format="%Y%m%d_%H%M%S"), ".csv", sep="")
+    },
+    content = function(file) {
+      write.csv(v$runs, file)
+    }
+  )
+
+
+  output$outruns <- renderDataTable(v$runs)
 
   observeEvent(input$xvariable,
                {v$xvalues<-input$xvariable;},
@@ -196,6 +265,10 @@ server <- function(input, output) {
   observeEvent(input$outid,
                {v$outid<-input$outid;},
                ignoreInit=TRUE)
+  observeEvent(input$queue,
+               {if(!is.null(isolate(v$runs))) {
+                 isolate(v$runs$hasdata <-FALSE)
+               }})
 
 
 }
